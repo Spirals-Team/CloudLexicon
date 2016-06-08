cef060e67f434fdc11f6c876e9e7ac38613c21db
Fabio Petrillo <fabio@petrillo.com>
2016-06-08 06:09:32 -0400

    OCCI dataset with provider in CSVs

--- tools/OCCURI2Resource.py
+++ tools/OCCURI2Resource.py
@@ -9,10 +9,11 @@ provider = 'OCCI'
 with open(file, 'rU') as f:
      freader = csv.reader(f, delimiter = ',', quoting=csv.QUOTE_NONE)
      for row in freader:
-	uri = row[0]     	
-	httpMethod = row[1]
-	service = row[2]
-	reference = row[3]
+	provider = row[0]
+	service = row[1]
+	uri = row[2]     	
+	httpMethod = row[3]
+	reference = row[4]
 
 	#Analyse resources and actions in the URI	
 	tokens = uri.split('/',100)
