b83d7d406f6a4a0ed6e8ddea4ed7f82da42e6da5
Fabio Petrillo <fabio@petrillo.com>
2016-06-14 17:53:59 -0400

    Google Cloud Platform - Scripts and dataset

---
+++ tools/GoogleExtractURIs.py
@@ -0,0 +1,45 @@
+import csv
+import sys
+import requests
+
+#Google Cloud Platform Documentation
+#Main source -> https://cloud.google.com/docs/
+
+#Line command CSV file argument
+file = sys.argv[1]
+
+with open(file, 'rU') as f:
+	provider = 'Google Cloud Platform'
+	freader = csv.reader(f, delimiter = ',', quoting=csv.QUOTE_NONE)
+	
+	for row in freader:
+		service = row[0]
+		reference = row[1]
+
+		resp = requests.get(url=reference)
+
+		for line in resp.iter_lines():
+			code = line.strip()
+			if '<code>' in code and code[0:6] == '<code>' and '</td>' not in code:
+				code = code[6:code.find('</code>')]
+				code = code.replace('&nbsp;','')
+				code = code.replace('<var class="apiparam">','')
+				code = code.replace('</var>','')
+				
+				httpMethod = code[0:code.find('/')].strip()
+				uri = code[code.find('/'):].strip()
+
+				#Analyse resources and actions in the URI	
+				tokens = uri.split('/',100)
+		
+				for token in tokens:
+					if ':' not in token:
+						#Resource 
+						print provider + ',' + service + ',' + 'RESOURCE' + ',' + token + ',' + httpMethod + ',' + uri + ',' + reference  
+					else:
+						#Action
+						print provider + ',' + service + ',' + 'ACTION' + ',' + token.split(':',1)[1] + ',' + httpMethod + ',' +  uri + ',' + reference
+			
+
+
+
--- tools/OCCURI2Resource.py
+++ tools/OCCURI2Resource.py
@@ -4,34 +4,27 @@ import sys
 #Line command CSV file argument
 file = sys.argv[1]
 
-provider = 'OCCI'
-
 with open(file, 'rU') as f:
-     freader = csv.reader(f, delimiter = ',', quoting=csv.QUOTE_NONE)
-     for row in freader:
-	provider = row[0]
-	service = row[1]
-	uri = row[2]     	
-	httpMethod = row[3]
-	reference = row[4]
+   freader = csv.reader(f, delimiter = ',', quoting=csv.QUOTE_NONE)
+   for row in freader:
+		provider = row[0]
+		service = row[1]
+		uri = row[2]     	
+		httpMethod = row[3]
+		reference = row[4]
 
-	#Analyse resources and actions in the URI	
-	tokens = uri.split('/',100)
+		#Analyse resources and actions in the URI	
+		tokens = uri.split('/',100)
 		
-	for token in tokens:
-	  if '?action' not in token:
-          #Resource 
-	    if token == '{provider_specific_path}' or token == '':
-	      continue
-	    print provider + ',' + service + ',' + 'RESOURCE' + ',' + token + ',' + httpMethod + ',' + uri + ',' + reference  
- 	  else :
-	  #Action
-	    print provider + ',' + service + ',' + 'ACTION' + ',' + token.split('=',1)[1] + ',' + httpMethod + ',' +  uri + ',' + reference  
-	
-
-  #print('Exception: it has subresources!')
-  #exit(-1)
-
+		for token in tokens:
+			if '?action' not in token:
+   			#Resource 
+	   	 	if token == '{provider_specific_path}' or token == '':
+	   	   	continue
+			 	print provider + ',' + service + ',' + 'RESOURCE' + ',' + token + ',' + httpMethod + ',' + uri + ',' + reference  
+			else:
+	  			#Action
+	    		print provider + ',' + service + ',' + 'ACTION' + ',' + token.split('=',1)[1] + ',' + httpMethod + ',' +  uri + ',' + reference
 	
 	
 	
